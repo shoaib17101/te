946debbe30e05fae039b977eb7e72130fbfcd917
shoaib <shoaibkhan171015393@gmail.com>
2025-11-13 18:08:39 +0500

    add product details

--- src/assets/Pages/Productdetailpage.jsx
+++ src/assets/Pages/Productdetailpage.jsx
@@ -9,28 +9,28 @@ export default function ProductDetailPage(){
     var category=''
 const {handleId}=useParams()
 const product=Products.filter(pr=>pr.handleId===handleId).map((pr)=>(
-    <section className='flex justify-center gap-4 p-10 '>
-        <div className='w-1/3'>
-     <div key={pr.handleId} className="p-3 gap ">
+    <section className='flex md:flex-row flex-col justify-center gap-4 p-10 '>
+        <div className='md:w-1/3'>
+     <div key={pr.handleId} className="p-3 gap order-2 md:order-1 ">
             <img src={"https://static.wixstatic.com/media/" + pr.productImageUrl} alt="" className="" />
 
                         <p className="text-md py-5">{pr.description}</p>
         </div>
         </div>
-        <div className="w-1/2 flex flex-col gap-3">
+        <div className="md:w-1/2 flex flex-col w-full  gap-3">
             <h2 className="font-bold text-3xl">{pr.name}</h2>
             <p>SKU: {pr.sku}</p>
             <h2 className="text-lg font-bold text-blue-500 " >{pr.price}$</h2>
             <h2>Quantity</h2>
-            <span className="join flex bg-white items-center justify-between w-1/6"><button onClick={()=>quantity!=0 && setquantity(q=>q-1)} className="btn btn-ghost">-</button > <p className='join-item'>{quantity}</p><button onClick={()=>setquantity(q=>q+1)} className="btn btn-ghost">+</button></span>
-            <button className="btn btn-primary px-10 p-5 w-1/3 rounded-full" >Add to Cart</button>
-            <button className="btn btn-primary px-10 p-5 w-1/3 rounded-full text-white bg-black">Buy Now</button>
+            <span className="join flex bg-white items-center justify-between md:w-1/6"><button onClick={()=>quantity!=0 && setquantity(q=>q-1)} className="btn btn-ghost">-</button > <p className='join-item'>{quantity}</p><button onClick={()=>setquantity(q=>q+1)} className="btn btn-ghost">+</button></span>
+            <button className="btn btn-primary px-10 p-5 md:w-1/3 rounded-full" >Add to Cart</button>
+            <button className="btn btn-primary px-10 p-5 md:w-1/3 rounded-full text-white bg-black">Buy Now</button>
             <h2>Product Info</h2>
-            <details className='w-1/3'>{pr.additionalInfoDescription1}</details>
+            <details className='md:w-1/3'>{pr.additionalInfoDescription1}</details>
             <h2>Refund  Policy</h2>
-            <details className='w-1/3'>{pr.additionalInfoDescription2}</details>
+            <details className='md:w-1/3'>{pr.additionalInfoDescription2}</details>
             <h2>Shipping Info</h2>
-            <details className='w-1/3'>{pr.additionalInfoDescription3}</details>
+            <details className='md:w-1/3 w-1/2'>{pr.additionalInfoDescription3}</details>
             {category=pr.collection}
         </div>
     </section>
